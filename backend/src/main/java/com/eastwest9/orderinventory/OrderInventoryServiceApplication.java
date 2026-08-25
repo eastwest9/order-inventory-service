419da71ef411ae0e132f97c4e72c83e4091be526
eastwest9 <dong092276@gmail.com>
2026-08-26 01:42:43 +0900

    chore(common): 애플리케이션 기본 시간대를 UTC로 설정

--- backend/src/main/java/com/eastwest9/orderinventory/OrderInventoryServiceApplication.java
+++ backend/src/main/java/com/eastwest9/orderinventory/OrderInventoryServiceApplication.java
@@ -1,5 +1,6 @@
 package com.eastwest9.orderinventory;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderInventoryServiceApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(OrderInventoryServiceApplication.class, args);
     }
 }
